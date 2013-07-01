7d4cda2526c818e601db4047a7635e44d77deba9
availlant <vmjacotte60>
2013-07-01 10:53:23 +0200

    ihm

---
+++ SoundSelector/SoundSelector/MainForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoundSelector
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void buttonParcourirFileToSearch_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Filter = "All Supported Audio | *.mp3; *.wma | MP3s | *.mp3 | WMAs | *.wma";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Multiselect = false;
+            openFileDialog.FileName = "";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFile.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void buttonParcourirFolder_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.Description = "Sélectionnez le dossier dans lequel se fera la recherche";
+            folderBrowserDialog.ShowNewFolderButton = false;
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFolder.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+    }
+}
