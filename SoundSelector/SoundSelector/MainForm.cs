d5840d1d04a3e407551e0e59d9af80cf39f8ffa9
availlant <vmjacotte60>
2013-07-07 17:38:04 +0200

    suppression

--- SoundSelector/SoundSelector/MainForm.cs
+++ SoundSelector/SoundSelector/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 using Business;
 
 namespace SoundSelector
@@ -22,14 +23,78 @@ namespace SoundSelector
         {
             dataGridViewResults.Columns.Add("collection", "Numéro");
             dataGridViewResults.Columns.Add("path", "Fichier");
-            dataGridViewResults.Columns.Add("lire", "Lecture");
-            dataGridViewResults.Columns.Add("supprimer", "Suppression");
+
+            MyDataGridViewButtonColumn columnLire = new MyDataGridViewButtonColumn();
+            columnLire.Name = "lire";
+            columnLire.Text = "Lire";
+            columnLire.HeaderText = "Lecture";
+            columnLire.UseColumnTextForButtonValue = true;
+            columnLire.cellClick += columnLire_cellClick;
+
+            dataGridViewResults.Columns.Add(columnLire);
+
+            MyDataGridViewButtonColumn columnSupp = new MyDataGridViewButtonColumn();
+            columnSupp.Name = "supp";
+            columnSupp.Text = "Supprimer";
+            columnSupp.HeaderText = "Suppression";
+            columnSupp.UseColumnTextForButtonValue = true;
+            columnSupp.cellClick += columnSupp_cellClick;
+
+            dataGridViewResults.Columns.Add(columnSupp);
 
             dataGridViewResults.EditMode = DataGridViewEditMode.EditProgrammatically;
             dataGridViewResults.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
             dataGridViewResults.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
             dataGridViewResults.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
             dataGridViewResults.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+        }
+
+        void columnSupp_cellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridViewResults.Rows[e.RowIndex].Cells[1].Value == null)
+                return;
+
+            string filePath = dataGridViewResults.Rows[e.RowIndex].Cells[1].Value.ToString();
+
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("Le fichier n'existe plus", "Fichier introuvable");
+            }
+            else
+            {
+                if (MessageBox.Show("Êtes-vous sûr de vouloir supprimer le fichier ?", "Suppression", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    try
+                    {
+                        File.Delete(filePath);
+                        MessageBox.Show("Le fichier a été supprimé", "Suppression");
+                    }
+                    catch (Exception exception)
+                    {
+                        MessageBox.Show("Le fichier n'a pas pu être supprimer", "Erreur");
+                    }
+                    catch (UnauthorizedAccessException exception)
+                    {
+                        MessageBox.Show("L'application n'a pas les droits pour supprimer le fichier", "Erreur");
+                    }
+                }
+            }
+        }
+
+        void columnLire_cellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridViewResults.Rows[e.RowIndex].Cells[1].Value == null)
+                return;
+
+            string filePath = dataGridViewResults.Rows[e.RowIndex].Cells[1].Value.ToString();
+
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("Le fichier n'existe plus", "Fichier introuvable");
+            }
+            else
+            {
+            }
         }
 
         private void PopulateGrid(Dictionary<string, List<string>> results)
@@ -148,5 +213,23 @@ namespace SoundSelector
         }
 
         #endregion        
+
+        private void dataGridViewResults_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridViewResults.Columns[e.ColumnIndex] is MyDataGridViewButtonColumn)
+            {
+                ((MyDataGridViewButtonColumn)dataGridViewResults.Columns[e.ColumnIndex]).RaiseEventCellClick(sender, e);
+            }
+        }
+    }
+
+    public class MyDataGridViewButtonColumn : DataGridViewButtonColumn
+    {
+        public event DataGridViewCellEventHandler cellClick;
+
+        public virtual void RaiseEventCellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            cellClick(sender, e);
+        }
     }
 }
